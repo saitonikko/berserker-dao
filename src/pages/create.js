544f28e71a9041d9060d7d9b8622e54e47e3e931
CFWood-github <cfwood1633@gmail.com>
2022-07-31 07:25:41 +0800

    update create ui

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { getProposalContract } from '../utils/contractFunctions';
-import {TOKEN_ADDR} from "../contracts/address";
+import { getProposalContract, getNFTBalance } from '../utils/contractFunctions';
+import { TOKEN_ADDR } from "../contracts/address";
 import '../assets/style/create.scss';
 import Medium from "../assets/img/medium.png"
 import Telegram from "../assets/img/telegram.png"
@@ -14,6 +14,8 @@ import Eargle from "../assets/img/eargle.png"
 
 
 export default function Create({ account }) {
+
+  const [NFTBalance, setNFTBalance] = useState(0);
 
   const [proposalName, setProposalName] = useState('')
   const [proposalDescription, setProposalDescription] = useState('')
@@ -29,7 +31,7 @@ export default function Create({ account }) {
     try {
       const contract = await getProposalContract();
       console.log(contract);
-      await contract.methods.propose(proposalTokenAddress, proposalTargetAddress, `0x${(Number(proposalValue)*1e18).toString(16)}`, proposalName, proposalDescription).send({from : account});
+      await contract.methods.propose(proposalTokenAddress, proposalTargetAddress, `0x${(Number(proposalValue) * 1e18).toString(16)}`, proposalName, proposalDescription).send({ from: account });
       alert('Successfully submitted');
     } catch (err) {
       alert('Something went wrong!');
@@ -37,44 +39,78 @@ export default function Create({ account }) {
     }
   }
 
+  useEffect(async () => {
+    if (!account) return;
+    const _NFTBalance = getNFTBalance(account);
+    setNFTBalance(_NFTBalance);
+  })
+
   return (
     <div className='create'>
 
-      <div className='content'>
+      {
+        NFTBalance === 0
+          ? <>
+            <div className='content'>
+              <div className='left-box'>
+                <div className='avoid-proposal'>
+                  <div className='title'>YOU HAVE NOT PERMISSION TO CREATE PROPOSAL!</div>
+                </div>
+              </div>
 
-        <div className='left-box'>
-          <div className='submit-proposal'>
-            <div className='title'>HOW TO SUBMIT A PROPOSAL</div>
-            <div className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit ultrices ut consectetur at est adipiscing blandit. Mi sit augue cursus nam nisl amet amet venenatis purus.</div>
-            <div className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit ultrices ut consectetur at est adipiscing blandit. Mi sit augue cursus nam nisl amet amet venenatis purus.</div>
-            <input className='name-input' placeholder='What is the name of the project?' onChange={(e) => setProposalName(e.target.value)}></input>
-            <textarea className='description-input' placeholder='Provide a short description of the project.' onChange={(e) => setProposalDescription(e.target.value)}></textarea>
-          </div>
-        </div>
-
-        <div className='right-box'>
-          <img className='bird' src={Bird} alt=''></img>
-          <img className='berserkers' src={Berserkers} alt=''></img>
-          <div className='right-absolute-box'>
-            <div className='create-proposal'>
-              <input className='proposal-value' placeholder='What is the value of the project?' onChange={(e) => setProposalValue(e.target.value)} ></input>
-              <input className='proposal-taget' placeholder='What is the address of the target?' onChange={(e) => setProposlTargetAddress(e.target.value)}></input>
-              <input className='token-address' value={TOKEN_ADDR} placeholder='What is the address of the token?' onChange={(e) => setProposalTokenAddress(e.target.value)}></input>
-              <div className='submit-box' onClick={createProposal}><img className='submit' alt='' src={Submit}></img></div>
+              <div className='right-box'>
+                <img className='bird' src={Bird} alt=''></img>
+                <img className='berserkers' src={Berserkers} alt=''></img>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
 
-      <div className='footer'>
-        <Link to='/proposals'><img alt='' src={BackButton} className='backbutton'></img></Link>
-        <img className='eargle' src={Eargle} alt=''></img>
-        <div className='link-box'>
-          <a><img alt='' src={Medium} className='link-icon'></img></a>
-          <a><img alt='' src={Telegram} className='link-icon'></img></a>
-          <a><img alt='' src={Twitter} className='link-icon'></img></a>
-        </div>
-      </div>
+            <div className='footer-avoid'>
+              <Link to='/proposals'><img alt='' src={BackButton} className='backbutton'></img></Link>
+              <img className='eargle' src={Eargle} alt=''></img>
+              <div className='link-box'>
+                <a><img alt='' src={Medium} className='link-icon'></img></a>
+                <a><img alt='' src={Telegram} className='link-icon'></img></a>
+                <a><img alt='' src={Twitter} className='link-icon'></img></a>
+              </div>
+            </div>
+          </>
+          : <>
+            <div className='content'>
+              <div className='left-box'>
+                <div className='submit-proposal'>
+                  <div className='title'>HOW TO SUBMIT A PROPOSAL</div>
+                  <div className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit ultrices ut consectetur at est adipiscing blandit. Mi sit augue cursus nam nisl amet amet venenatis purus.</div>
+                  <div className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit ultrices ut consectetur at est adipiscing blandit. Mi sit augue cursus nam nisl amet amet venenatis purus.</div>
+                  <input className='name-input' placeholder='What is the name of the project?' onChange={(e) => setProposalName(e.target.value)}></input>
+                  <textarea className='description-input' placeholder='Provide a short description of the project.' onChange={(e) => setProposalDescription(e.target.value)}></textarea>
+                </div>
+              </div>
+
+              <div className='right-box'>
+                <img className='bird' src={Bird} alt=''></img>
+                <img className='berserkers' src={Berserkers} alt=''></img>
+                <div className='right-absolute-box'>
+                  <div className='create-proposal'>
+                    <input className='proposal-value' placeholder='What is the value of the project?' onChange={(e) => setProposalValue(e.target.value)} ></input>
+                    <input className='proposal-taget' placeholder='What is the address of the target?' onChange={(e) => setProposlTargetAddress(e.target.value)}></input>
+                    <input className='token-address' value={TOKEN_ADDR} placeholder='What is the address of the token?' onChange={(e) => setProposalTokenAddress(e.target.value)}></input>
+                    <div className='submit-box' onClick={createProposal}><img className='submit' alt='' src={Submit}></img></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='footer'>
+              <Link to='/proposals'><img alt='' src={BackButton} className='backbutton'></img></Link>
+              <img className='eargle' src={Eargle} alt=''></img>
+              <div className='link-box'>
+                <a><img alt='' src={Medium} className='link-icon'></img></a>
+                <a><img alt='' src={Telegram} className='link-icon'></img></a>
+                <a><img alt='' src={Twitter} className='link-icon'></img></a>
+              </div>
+            </div>
+          </>
+      }
 
     </div>
   )
--- src/utils/contractFunctions.js
+++ src/utils/contractFunctions.js
@@ -1,8 +1,13 @@
 import Web3 from 'web3';
-import { CLAIM_ADDR, GOVERNANCE_ADDR } from "../contracts/address"
-import { CLAIM_ABI, GOVERNANCE_ABI } from '../contracts/abi';
+import { BERSERKER_ADDR, CLAIM_ADDR, GOVERNANCE_ADDR } from "../contracts/address"
+import { BERSERKER_ABI, CLAIM_ABI, GOVERNANCE_ABI } from '../contracts/abi';
 
 const web3 = new Web3(window.ethereum);
+
+export const getBerserkerContract = async () => {
+  const contract = await new web3.eth.Contract(BERSERKER_ABI, BERSERKER_ADDR);
+  return contract;
+}
 
 export const getClaimContract = async () => {
   // if (signer)
@@ -35,5 +40,12 @@ export const getBalance = async (_account) => {
   if (!_account) return 0;
   const _balance = await web3.eth.getBalance(_account);
   return web3.utils.fromWei(_balance, "ether");
+}
+
+export const getNFTBalance = async (_account) => {
+  if (!_account) return 0;
+  const contract = await getBerserkerContract();
+  const _balance = await contract.methods.balanceOf(_account).call();
+  return _balance;
 }
 
